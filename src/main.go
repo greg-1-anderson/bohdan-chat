f595b7ddb6f5067f2ed27d5e4592fc93e127265b
Greg Anderson <greg.1.anderson@greenknowe.org>
2019-04-29 14:50:34 -0700

    Simple bot experiment that rolls dice

--- src/main.go
+++ src/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"regexp"
+	"strconv"
 
 	"github.com/gorilla/websocket"
 )
@@ -35,9 +39,9 @@ func main() {
 	// Start listening for incoming chat messages
 	go handleMessages()
 
-	// Start the server on localhost port 8000 and log any errors
-	log.Println("http server started on :8000")
-	err := http.ListenAndServe(":8000", nil)
+	// Start the server on localhost port 8001 and log any errors
+	log.Println("http server started on :8001")
+	err := http.ListenAndServe(":8001", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
@@ -74,13 +78,71 @@ func handleMessages() {
 		// Grab the next message from the broadcast channel
 		msg := <-broadcast
 		// Send it out to every client that is currently connected
-		for client := range clients {
-			err := client.WriteJSON(msg)
-			if err != nil {
-				log.Printf("error: %v", err)
-				client.Close()
-				delete(clients, client)
-			}
+		sendMessage(msg)
+		processMessageForChatOps(msg)
+	}
+}
+
+func processMessageForChatOps(msg Message) {
+	var isRollBot = regexp.MustCompile(`^@roll `)
+	if isRollBot.MatchString(msg.Message) {
+		processRollBot(msg.Message[6:])
+		return
+	}
+}
+
+func processRollBot(whatToRoll string) {
+	re := regexp.MustCompile("([0-9]+)?d([0-9]+)([\\+-])?([0-9]+)?")
+	dice := re.FindStringSubmatch(whatToRoll)
+	if len(dice) == 0 {
+		sendMessageFromBot("Error: cannot roll " + whatToRoll)
+		return
+	}
+	rollDice(dice[1], dice[2], dice[3], dice[4])
+}
+
+func rollDice(number string, sides string, sign string, addition string) {
+	n := 1
+	if len(number) > 0 {
+		n, _ = strconv.Atoi(number)
+	}
+	s, _ := strconv.Atoi(sides)
+
+	total := randomDice(n, s)
+
+	if len(addition) > 0 {
+		a, _ := strconv.Atoi(addition)
+		total = total + a
+	}
+
+	// var result = fmt.Sprintf("%s %s %s", number, sides, addition)
+	result := fmt.Sprintf("%d", total)
+	sendMessageFromBot(result)
+}
+
+func randomDice(n int, s int) int {
+	total := 0
+
+	for i := 0; i < n; i++ {
+		roll := rand.Intn(s) + 1
+		total = total + roll
+	}
+
+	return total
+}
+
+func sendMessageFromBot(message string) {
+	msg := Message{Email: "bot@getpantheon.com", Username: "Bot", Message: message}
+	sendMessage(msg)
+}
+
+func sendMessage(msg Message) {
+	for client := range clients {
+		err := client.WriteJSON(msg)
+		if err != nil {
+			log.Printf("error: %v", err)
+			client.Close()
+			delete(clients, client)
 		}
 	}
 }
